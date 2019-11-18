f724e85874702bb9eac30ab4730efb1ced6e824d
dnowo <danielnowo@gmail.com>
2019-11-18 13:42:26 +0100

    Push copy constructor. Edit directories for Z4.

---
+++ Copy constructor C++/Z5/Z5/Box.cpp
@@ -0,0 +1,52 @@
+#include "Box.h"
+int Box::Nr = -1;
+int Box::Nrkopii = -1;
+Box::Box()
+{
+	Nr++;
+	Nazwa += "i" + to_string(this->Nr);
+	objetosc = 1;
+	cout << "K: tworze " << Nazwa << " -def" << endl;
+}
+
+Box::Box(string _nazwa)
+{
+	Nr++;
+	Nazwa = "i" + to_string(Nr)+ " " + _nazwa;
+	objetosc = 1;
+	cout << "K: tworze " << Nazwa << " -def" << endl;
+}
+
+Box::Box(string _nazwa, double _x)
+{
+	Nr++;
+	Nazwa = "i" + to_string(Nr) + " " + _nazwa;
+	objetosc = _x;
+	
+	cout << "K: tworze " << Nazwa << " -def" << endl;
+}
+
+Box::Box(string _nazwa, double _x, double _y, double _z)
+{
+	Nr++;
+	Nazwa = "i" + to_string(Nr) + " " + _nazwa;
+	objetosc = _x*_y*_z;
+	cout << "K: tworze " << Nazwa << " -def" << endl;
+
+}
+
+Box::Box(const Box& box)
+{
+	Nr++;
+	Nrkopii++;
+	objetosc = box.objetosc;
+	Nazwazrodlowegoobiektu = box.Nazwa;
+	Nazwa = "i" + to_string(Nr) + " -kopia" + to_string(Nrkopii) + "_" + Nazwazrodlowegoobiektu;
+	cout << "KK: tworze " << Nazwa << endl;
+
+}
+
+void Box::printVolume()
+{
+	cout << "V(" << Nazwa << ")=" << objetosc << endl;
+}
